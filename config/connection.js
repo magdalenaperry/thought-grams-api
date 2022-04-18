33d9975a10ba63b1c48aa849372f1567de8576c5
Magdalena Perry <magperry16@gmail.com>
2022-04-18 14:52:04 -0400

    initial setup

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+const {
+  connect,
+  connection
+} = require('mongoose');
+
+connect('mongodb://localhost/usersPosts', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = connection;
